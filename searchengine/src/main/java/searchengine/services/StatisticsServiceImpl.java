b5fcbec1e67105ef595f9c1925a75fc8e5a38abf
Станислав Егоров <savvvva@mail.ru>
2023-04-16 22:20:36 +0300

    Correct getStatistics()

--- searchengine/src/main/java/searchengine/services/StatisticsServiceImpl.java
+++ searchengine/src/main/java/searchengine/services/StatisticsServiceImpl.java
@@ -2,12 +2,12 @@ package searchengine.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import searchengine.config.Site;
 import searchengine.config.IndexingSettings;
 import searchengine.dto.statistics.DetailedStatisticsItem;
 import searchengine.dto.statistics.StatisticsData;
 import searchengine.dto.statistics.StatisticsResponse;
 import searchengine.dto.statistics.TotalStatistics;
+import searchengine.model.Site;
 import searchengine.repositories.LemmaRepository;
 import searchengine.repositories.PageRepository;
 import searchengine.repositories.SiteRepository;
@@ -18,60 +18,60 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class StatisticsServiceImpl implements StatisticsService {
-
-    private final LemmaRepository lemmaRepository;
+    private final IndexingSettings indexingSettings;
 
     private final SiteRepository siteRepository;
 
     private final PageRepository pageRepository;
 
-    private final Random random = new Random();
-    private final IndexingSettings sites;
+    private final LemmaRepository lemmaRepository;
 
     @Override
     public StatisticsResponse getStatistics() {
-        TotalStatistics total = new TotalStatistics();
-        total.setSites(sites.getSites().size());
-        total.setIndexing(true);
-        List<DetailedStatisticsItem> detailed = new ArrayList<>();
-        List<Site> sitesList = sites.getSites();
-        for (int i = 0; i < sitesList.size(); i++) {
-            searchengine.config.Site site = sitesList.get(i);
-            searchengine.model.Site modelSite =
-                    siteRepository.findSiteByUrl(site.getUrl());
-            int pages = pageRepository.countIndexedPage(modelSite.getId());
-            int lemmas = lemmaRepository.getCountLemmasBySiteId(modelSite.getId());
+        TotalStatistics totalStatistics = new TotalStatistics();
+        totalStatistics.setSites(indexingSettings.getSites().size());
+        totalStatistics.setIndexing(true);
 
-            DetailedStatisticsItem item = new DetailedStatisticsItem();
-            item.setUrl(site.getUrl());
-            item.setName(site.getName());
-            item.setStatus(modelSite.getType().toString());
-            item.setPages(pages);
-            item.setLemmas(lemmas);
-            item.setError(modelSite.getLastError()==null ? "" : modelSite.getLastError());
-            item.setStatusTime(getTimestamp(modelSite.getStatusTime()));
-            total.setPages(total.getPages() + pages);
-            total.setLemmas(total.getLemmas() + lemmas);
-            detailed.add(item);
+        List<DetailedStatisticsItem> detailedStatisticsItems = new ArrayList<>();
+        List<searchengine.config.Site> configSites = indexingSettings.getSites();
+        for (searchengine.config.Site configSite : configSites) {
+            Site site = siteRepository.findSiteByUrl(configSite.getUrl());
+            if (site == null) {
+                continue;
+            }
+            int pages = pageRepository.countIndexedPage(site.getId());
+            int lemmas = lemmaRepository.getCountLemmasBySiteId(site.getId());
+
+            DetailedStatisticsItem detailedStatisticsItem = new DetailedStatisticsItem();
+            detailedStatisticsItem.setUrl(configSite.getUrl());
+            detailedStatisticsItem.setName(configSite.getName());
+            detailedStatisticsItem.setStatus(site.getType().toString());
+            detailedStatisticsItem.setPages(pages);
+            detailedStatisticsItem.setLemmas(lemmas);
+            detailedStatisticsItem.setError(site.getLastError() == null ? "" : site.getLastError());
+            detailedStatisticsItem.setStatusTime(getTimestamp(site.getStatusTime()));
+            detailedStatisticsItems.add(detailedStatisticsItem);
+
+            totalStatistics.setPages(totalStatistics.getPages() + pages);
+            totalStatistics.setLemmas(totalStatistics.getLemmas() + lemmas);
         }
 
-        StatisticsResponse response = new StatisticsResponse();
-        StatisticsData data = new StatisticsData();
-        data.setTotal(total);
-        data.setDetailed(detailed);
-        response.setStatistics(data);
-        response.setResult(true);
-        return response;
+        StatisticsData statisticsData = new StatisticsData();
+        statisticsData.setTotal(totalStatistics);
+        statisticsData.setDetailed(detailedStatisticsItems);
+        StatisticsResponse statisticsResponse = new StatisticsResponse();
+        statisticsResponse.setStatistics(statisticsData);
+        statisticsResponse.setResult(true);
+        return statisticsResponse;
     }
 
     private long getTimestamp(String date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyy hh:mm:ss");
-        Date parsedDate = null;
+        Date parsedDate;
         try {
             parsedDate = dateFormat.parse(date);
         } catch (ParseException e) {
